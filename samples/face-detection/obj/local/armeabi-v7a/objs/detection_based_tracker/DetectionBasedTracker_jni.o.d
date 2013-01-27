dca6d7ad1479b14dbc24f9ea8a9228967cb9463e
Nehil Jain <jain.nehil@gmail.com>
2013-01-27 14:14:40 -0500

    initial commit

---
+++ samples/face-detection/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,142 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/jni.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/assert.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/string.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/math.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/jni.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/log.h:
---
+++ samples/tutorial-4-mixed/obj/local/armeabi-v7a/objs/mixed_sample/jni_part.o.d
@@ -0,0 +1,148 @@
+obj/local/armeabi-v7a/objs/mixed_sample/jni_part.o: jni/jni_part.cpp \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/jni.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/assert.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/string.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/math.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ ../../sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ ../../sdk/native/jni/include/opencv2/core/core_c.h \
+ ../../sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ ../../sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ ../../sdk/native/jni/include/opencv2/flann/defines.h \
+ ../../sdk/native/jni/include/opencv2/flann/config.h
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/jni.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/Users/nehilj/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/nehilj/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+../../sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+../../sdk/native/jni/include/opencv2/core/core_c.h:
+
+../../sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+../../sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+../../sdk/native/jni/include/opencv2/flann/defines.h:
+
+../../sdk/native/jni/include/opencv2/flann/config.h:
